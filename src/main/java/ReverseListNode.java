82d51a6bebcaa159bcd2895f37d98c3543760d31
chris <chris0er@qq.com>
2021-09-18 22:07:54 +0800

    modify

--- src/main/java/ReverseListNode.java
+++ src/main/java/ReverseListNode.java
@@ -18,10 +18,14 @@ public class ReverseListNode {
 
         printNode(listNode1);
         LOG.info("");
-        ListNode listNode = reverse(listNode1);
+        //ListNode listNode = reverse(listNode1);
+        ListNode listNode = recursion(listNode1);
         printNode(listNode);
     }
 
+    /**
+     * 遍历并反转链表
+     */
     public static ListNode reverse(ListNode head) {
         ListNode previousNode = null;
         ListNode currentNode = null;
@@ -45,6 +49,21 @@ public class ReverseListNode {
             currentNode.next = nextNode;
         }
         return currentNode;
+    }
+
+    /**
+     * 通过递归反转链表
+     * [1| ] -> [2| ] -> [3| ] -> [4| ] ->  [5| null ]
+     * [5| ] -> [4| ] -> [3| ] -> [2| ] ->  [1| null ]
+     */
+    public static ListNode recursion(ListNode node) {
+        if (node == null) {
+            return null;
+        }
+        ListNode newNode = null;
+        newNode = reverse(node.next);
+        newNode.next = node;
+        return newNode;
     }
 
     /**
