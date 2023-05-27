815352842072fa81339eaf44e5e2faefb7f373c7
chris <chris>
2023-05-28 02:04:04 +0800

    edit

--- src/main/java/com/demo/Application.java
+++ src/main/java/com/demo/Application.java
@@ -1,12 +1,14 @@
 package com.demo;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author chris
  * @create 2023/5/28
  */
-//@Slf4j
+@Slf4j
 public class Application {
     public static void main(String[] args) {
-//        log.info("main");
+        log.info("main");
     }
 }
