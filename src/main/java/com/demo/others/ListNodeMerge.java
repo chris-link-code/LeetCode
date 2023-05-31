6fba1fc67b2f32b95766ef430f8fbbbe8c6ddafc
chris <er1293545211@live.com>
2023-05-31 17:05:40 +0800

    merge ListNode

--- src/main/java/com/demo/others/ListNodeMerge.java
+++ src/main/java/com/demo/others/ListNodeMerge.java
@@ -52,6 +52,7 @@ public class ListNodeMerge {
             }
             p = p.next;
         }
+        // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
         if (headA != null) {
             p.next = headA;
         }
