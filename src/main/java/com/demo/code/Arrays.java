f2d92c9106e3b77071615c115b79920a4c817209
chris <er1293545211@live.com>
2023-06-15 17:57:27 +0800

    merge alternately

--- src/main/java/com/demo/code/Arrays.java
+++ src/main/java/com/demo/code/Arrays.java
@@ -1,6 +1,7 @@
 package com.demo.code;
 
 import com.demo.util.Utils;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * @author chris
@@ -261,5 +262,62 @@ public class Arrays {
             System.out.println(number + "\t" + candidate + "\t" + count);
         }
         return candidate;
+    }
+
+    /**
+     * 1768. 交替合并字符串
+     * 给你两个字符串 word1 和 word2 。请你从 word1 开始，通过交替添加字母来合并字符串。
+     * 如果一个字符串比另一个字符串长，就将多出来的字母追加到合并后字符串的末尾。
+     * 返回 合并后的字符串 。
+     * <p>
+     * 示例 1：
+     * 输入：word1 = "abc", word2 = "pqr"
+     * 输出："apbqcr"
+     * 解释：字符串合并情况如下所示：
+     * word1：  a   b   c
+     * word2：    p   q   r
+     * 合并后：  a p b q c r
+     * 示例 2：
+     * 输入：word1 = "ab", word2 = "pqrs"
+     * 输出："apbqrs"
+     * 解释：注意，word2 比 word1 长，"rs" 需要追加到合并后字符串的末尾。
+     * word1：  a   b
+     * word2：    p   q   r   s
+     * 合并后：  a p b q   r   s
+     * 示例 3：
+     * 输入：word1 = "abcd", word2 = "pq"
+     * 输出："apbqcd"
+     * 解释：注意，word1 比 word2 长，"cd" 需要追加到合并后字符串的末尾。
+     * word1：  a   b   c   d
+     * word2：    p   q
+     * 合并后：  a p b q c   d
+     */
+    private static String mergeAlternately(String word1, String word2) {
+        if (StringUtils.isEmpty(word1)) {
+            return word2;
+        }
+        if (StringUtils.isEmpty(word2)) {
+            return word1;
+        }
+
+        char[] array1 = word1.toCharArray();
+        char[] array2 = word2.toCharArray();
+
+        int length1 = array1.length;
+        int length2 = array2.length;
+        int index1 = 0;
+        int index2 = 0;
+        StringBuilder sb = new StringBuilder();
+        while (index1 < length1 || index2 < length2) {
+            if (index1 < length1) {
+                sb.append(array1[index1]);
+                index1++;
+            }
+            if (index2 < length2) {
+                sb.append(array2[index2]);
+                index2++;
+            }
+        }
+        return sb.toString();
     }
 }
