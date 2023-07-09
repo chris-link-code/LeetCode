c843bc051c9e1438bcd15902ea12d77241706709
chris <chris>
2023-07-09 21:56:12 +0800

    increasing triplet

--- src/main/java/com/demo/code/Arrays.java
+++ src/main/java/com/demo/code/Arrays.java
@@ -426,4 +426,46 @@ public class Arrays {
         }
         return answer;
     }
+
+    /**
+     * 334. 递增的三元子序列
+     * 给你一个整数数组 nums ，判断这个数组中是否存在长度为 3 的递增子序列。
+     * 如果存在这样的三元组下标 (i, j, k) 且满足i < j < k，
+     * 使得 nums[i] < nums[j] < nums[k]，
+     * 返回 true ；否则，返回 false 。
+     * 示例 1：
+     * 输入：nums = [1,2,3,4,5]
+     * 输出：true
+     * 解释：任何 i < j < k 的三元组都满足题意
+     * 示例 2：
+     * 输入：nums = [5,4,3,2,1]
+     * 输出：false
+     * 解释：不存在满足题意的三元组
+     * 示例 3：
+     * 输入：nums = [2,1,5,0,4,6]
+     * 输出：true
+     * 解释：三元组 (3, 4, 5) 满足题意，
+     * 因为 nums[3] == 0 < nums[4] == 4 < nums[5] == 6
+     */
+    private static boolean increasingTriplet(Integer[] array) {
+        boolean increase = false;
+        boolean last = false;
+        int i = 1;
+        int length = array.length;
+        if (length < 3) {
+            return false;
+        }
+        while (i < length) {
+            if (array[i - 1] < array[i]) {
+                if (last) {
+                    return true;
+                }
+                last = true;
+            } else {
+                last = false;
+            }
+            i++;
+        }
+        return false;
+    }
 }
